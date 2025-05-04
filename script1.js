1a7db01a17b4f89ca2609d2237b6cc47829e3a06
Harsh kumar pandey <harshpandey12378@gmail.com>
2025-05-04 21:45:39 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -300,7 +300,7 @@ document.addEventListener("DOMContentLoaded", function () {
                     break;
                  }
                  console.log("Current num:", currentNum);
-               if(arr[0]=="+"&&arr[0]=="-"&&arr[0]=="*"&&arr[0]=="/"){
+               if(arr[0]!="+"&&arr[0]!="-"&&arr[0]!="*"&&arr[0]!="/"){
                 currentNum=arr.shift();                                                                        
                 }                                                         //getting the first number from the array
                 num1=0;
